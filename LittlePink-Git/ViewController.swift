ed446f124e9a87016597b30ebba02274fa68cf1f
Warmingoug <warmingoug@gmail.com>
2023-07-26 22:52:03 +0800

    Initial Commit

---
+++ LittlePink-Git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LittlePink-Git
+//
+//  Created by 王鸣 on 2023/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
